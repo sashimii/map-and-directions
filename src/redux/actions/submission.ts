3d9ddc627ed4b1b81ce793958bcee08e132ca82a
Sushil Tailor <sushil.tailor@gmail.com>
2018-12-18 06:13:34 -0500

    minor cleanup

--- src/redux/actions/submission.ts
+++ src/redux/actions/submission.ts
@@ -3,6 +3,8 @@ import { actionTypes } from './actionTypes';
 
 const API_URI = `http://localhost:8080/route`;
 
+const tokenizeUri = token => `${API_URI}/${token}`;
+
 export const flagServerError = () => {
   return {
     type: actionTypes.FLAG_SERVER_ERROR,
@@ -47,39 +49,6 @@ export const updateDeliveryRouteInformation = (path, totalDistance, totalTime) =
     totalTime,
   };
 };
-
-// if (!dice) {
-//   this.status = 500
-//   return
-// }
-
-// if (dice === 1) {
-//   this.body = {
-//     status: 'in progress'
-//   }
-//   return
-// }
-
-// if (dice === 2) {
-//   this.body = {
-//     status: 'failure',
-//     error: 'Location not accessible by car'
-//   }
-//   return
-// }
-
-// this.body = {
-//   status: 'success',
-//   path: [
-//     ["22.372081", "114.107877"],
-//     ["22.326442", "114.167811"],
-//     ["22.284419", "114.159510"]
-//   ],
-//   total_distance: 20000,
-//   total_time: 1800
-// }
-
-const tokenizeUri = token => `${API_URI}/${token}`;
 
 export const postLocations = locations => dispatch => {
   return axios.post(API_URI, locations)
